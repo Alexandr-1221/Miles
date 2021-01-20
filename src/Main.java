1772f538d24458fd7dbddf18e87b3297756d826d
Alexandr-1221 <ags.sad.1221@gmail.com>
2021-01-20 10:36:35 +0300

    create repository

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 15000;
+        int costOfMile = 20;
+        int miles = cost / costOfMile;
+        System.out.println(miles);
+    }
+}
